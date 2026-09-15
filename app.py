0f019b828c9618e83ff6f928ea79d3869c537367
unknown <bmlien@ucsc.edu>
2026-09-14 20:07:48 -0700

    multi-file upload

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import shutil
 from pathlib import Path
 
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 PHOTOS_DIR = Path("/mnt/photos")
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/files", StaticFiles(directory=PHOTOS_DIR), names="files")
 
 @app.get("/", response_class=HTMLResponse)
 def home():
@@ -22,8 +26,9 @@ def home():
     """
 
 @app.post("/upload")
-def upload(file: UploadFile = File(...)):
-    dest = PHOTOS_DIR / file.filename
-    with open(dest, "wb") as f:
-        shutil.copyfileobj(file.file, f)
-    return {"filename" : file.filename, "status": "uploaded"}
+def upload(files: list[UploadFile] = File(...)):
+    for file in files:
+        dest = PHOTOS_DIR / file.filename
+        with open(dest, "wb") as f:
+            shutil.copyfileobj(file.file, f)
+    return HTMLResponse('<a href="/">Done — back to gallery</a>')
