37ecbe07d4cf23d61861b4186617937c6f2dfff4
unknown <bmlien@ucsc.edu>
2026-09-14 20:01:02 -0700

    add file upload endpoint

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
-from fastapi import FastAPI
+import shutil
+from pathlib import Path
 
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
+
+PHOTOS_DIR = Path("/mnt/photos")
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def home():
-    return {"message": "Photo server is running"}
+    return """
+    <html>
+    <body>
+      <h1>Upload a photo</h1>
+      <form action="/upload" method="post" enctype="multipart/form-data">
+        <input type="file" name="file">
+        <button type="submit">Upload</button>
+      </form>
+    </body>
+    </html>
+    """
+
+@app.post("/upload")
+def upload(file: UploadFile = File(...)):
+    dest = PHOTOS_DIR / file.filename
+    with open(dest, "wb") as f:
+        shutil.copyfileobj(file.file, f)
+    return {"filename" : file.filename, "status": "uploaded"}
